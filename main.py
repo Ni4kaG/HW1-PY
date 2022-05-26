7751bd754d52c4b048e3442180e49a6b9da97d7e
Veronika <VGZhidyaevskaya@google.com>
2022-05-26 16:38:32 +0300

    HW 1 Python Dev

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 
 
 def create_html(valutes):
-    text = '<h1>Курс валют</h1>'
+    text = '<h1>Курс валют от Ni4kaG</h1>'
     text += '<table>'
     text += '<tr>'
     for _ in valutes[0]:
